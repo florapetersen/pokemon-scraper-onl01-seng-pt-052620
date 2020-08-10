89d5e4594404a32f804de1eb7be87b5875f2eb77
Flora Petersen <floramarina@comcast.net>
2020-08-10 17:23:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ?
+    SQL
+    new_pokemon = db.execute(sql, id)
+    Pokemon.new(id, new_pokemon[1], new_pokemon[2], new_pokemon[3], db)
+  end
+
+
 end
