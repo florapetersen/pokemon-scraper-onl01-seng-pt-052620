f76c0c8d9a456267bfe9ed4208e3eae178518dfc
Flora Petersen <floramarina@comcast.net>
2020-08-10 17:38:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
     #sql = <<-SQL
       #SELECT * FROM pokemon WHERE id = ?
     #SQL
-    new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?");
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?");
     Pokemon.new(id, new_pokemon[1], new_pokemon[2], db)
   end
 
